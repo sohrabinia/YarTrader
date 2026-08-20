7ec13f949c1616e5c40ed6c9e07646b430c1793a
Mohammad Ali Sohrabinia <m.a.sohrabinia@gmail.com>
2026-08-21 00:50:49 +0330

    fix(runtime): resolve research runtime merge conflict

--- src/Application/Runtime/research_runtime.py
+++ src/Application/Runtime/research_runtime.py
@@ -33,10 +33,9 @@ class ResearchRuntime:
         from src.Application.Deployment.storage import YarTraderStorageManager
         storage_mgr = YarTraderStorageManager.get_manager()
 
-        if not evidence_dir:
-            self._evidence_dir = os.path.join(storage_mgr.get_runtime_dir(), "research_logs")
-        elif not os.path.isabs(evidence_dir):
-            self._evidence_dir = os.path.join(storage_mgr.get_runtime_dir(), evidence_dir)
+        if not evidence_dir or not os.path.isabs(evidence_dir):
+            sub_folder = os.path.basename(evidence_dir) if evidence_dir else "research_logs"
+            self._evidence_dir = os.path.join(storage_mgr.get_runtime_dir(), sub_folder)
         else:
             self._evidence_dir = evidence_dir
         self._history: List[ResearchResult] = []
